e8b0034e1f53d5fa87a08699e6e20f02b5f5bed7
Qntnroos <quinroos@student.arteveldehs.be>
2019-03-14 10:19:02 +0100

    added updated db

--- code_project/src/AppBundle/Controller/SeperateMovieController.php
+++ code_project/src/AppBundle/Controller/SeperateMovieController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use AppBundle\utils\database;
 
 class SeperateMovieController extends Controller
 {
@@ -13,7 +14,10 @@ class SeperateMovieController extends Controller
      */
     public function renderFilm(Request $request)
     {
+        $db = new database();
+        $values = $db-> getMovieDetails();
+        $val = ['info' => $values];
         // replace this example code with whatever you need
-        return $this->render('pages/seperate-film.html.twig');
+        return $this->render('pages/seperate-film.html.twig', $val);
     }
 }
