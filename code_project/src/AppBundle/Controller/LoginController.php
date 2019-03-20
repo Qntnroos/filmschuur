2c810ad00b84af8e12bc0746ada2f2aa1d658f9f
danny <danny.eeraerts@proximus.be>
2019-03-20 12:17:30 +0100

    Change login conroller

--- code_project/src/AppBundle/Controller/LoginController.php
+++ code_project/src/AppBundle/Controller/LoginController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\Session;
 use AppBundle\utils\database;
 
 class LoginController extends Controller
@@ -12,12 +13,14 @@ class LoginController extends Controller
     /**
      * @Route("/login", name="login")
      */
-    public function PasswordCheck(Request $request){
+    public function PasswordCheck(Request $request, Session $session){
+        $session = new Session();
+        $session->start();
         if($request->get('RegisterFormEmail') && (empty($request->get('RegisterFormEmail')))){
             $email1Err = "E-mail is vereist&nbsp;&#x274C;";
             $session->remove('email1');
+            return $email1Err;
         }
-     
         return $this->render('login/login.html.twig');
     }
 }
