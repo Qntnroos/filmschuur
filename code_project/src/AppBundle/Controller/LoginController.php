734f3716bd73c450a4bdc1e764f3c962fa40531a
danny <danny.eeraerts@proximus.be>
2019-03-20 09:37:24 +0100

    Login controller

---
+++ code_project/src/AppBundle/Controller/LoginController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LoginController extends Controller
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('login/login.html.twig');
+    }
+}
