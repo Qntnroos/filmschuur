fb9d3e6ab20312dd733084ce1408b4a1b5f13595
danny <danny.eeraerts@proximus.be>
2019-05-08 15:19:20 +0200

    Add RouteController

---
+++ code_project/src/AppBundle/Controller/RouteController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\Session;
+use AppBundle\utils\database;
+
+class LoginController extends Controller
+{
+    /**
+     * @Route("/route", name="route")
+     */
+}
