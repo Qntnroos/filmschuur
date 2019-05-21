211926d265d909f22245743219cb53ca83782f75
Qntnroos <quinroos@student.arteveldehs.be>
2019-05-21 22:16:12 +0200

    updated css and twig files

--- src/Controller/AlgemeneVoorwaardenController.php
+++ src/Controller/AlgemeneVoorwaardenController.php
@@ -5,15 +5,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Session\Session;
-use App\utils\database;
 
 class AlgemeneVoorwaardenController extends Controller
 {
     /**
      * @Route("/algemene_voorwaarden", name="Algemene Voorwaarden")
      */
-    public function route_to_algemene_voorwaarden_page(Request $request){
+    public function route_to_algemene_voorwaarden_page(Request $request)
+    {
     
         return $this->render('pages/algemene_voorwaarden.html.twig');
     }
