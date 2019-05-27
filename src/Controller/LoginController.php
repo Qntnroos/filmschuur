41c36bdc580b8e7575c327f351a34d60e3f3d2d8
Qntnroos <quinroos@student.arteveldehs.be>
2019-05-27 15:52:52 +0200

    updated controllers to abstract controller

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,13 +2,13 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Session\Session;
 use App\utils\database;
 
-class LoginController extends Controller
+class LoginController extends AbstractController
 {
     /**
      * @Route("/login", name="login")
