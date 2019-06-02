37fc05f9a6eca70edf5eaf21170fbcddd50cde29
danny <danny.eeraerts@proximus.be>
2019-06-02 17:18:59 +0200

    Add Entity Cities

---
+++ src/Entity/Cities.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CitiesRepository")
+ */
+class Cities
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=10, nullable=true)
+     */
+    private $postal_code;
+
+    /**
+     * @ORM\Column(type="string", length=85)
+     */
+    private $city_name;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\User", mappedBy="city")
+     */
+    private $users;
+
+    public function __construct()
+    {
+        $this->users = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPostalCode(): ?string
+    {
+        return $this->postal_code;
+    }
+
+    public function setPostalCode(?string $postal_code): self
+    {
+        $this->postal_code = $postal_code;
+
+        return $this;
+    }
+
+    public function getCityName(): ?string
+    {
+        return $this->city_name;
+    }
+
+    public function setCityName(string $city_name): self
+    {
+        $this->city_name = $city_name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|User[]
+     */
+    public function getUsers(): Collection
+    {
+        return $this->users;
+    }
+
+    public function addUser(User $user): self
+    {
+        if (!$this->users->contains($user)) {
+            $this->users[] = $user;
+            $user->setCity($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUser(User $user): self
+    {
+        if ($this->users->contains($user)) {
+            $this->users->removeElement($user);
+            // set the owning side to null (unless already changed)
+            if ($user->getCity() === $this) {
+                $user->setCity(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function __toString() {
+        // to show the postalcode of the City in the select
+        return $this->city_name;
+        // to show the postalcode of the City in the select
+    }
+}
+
