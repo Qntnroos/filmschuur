16f09edf2c9c4387fd207005a30eac4d89d9f29d
danny <danny.eeraerts@proximus.be>
2019-06-16 18:45:50 +0200

    Add auditorium data

--- src/Controller/TicketsOrderController.php
+++ src/Controller/TicketsOrderController.php
@@ -9,6 +9,8 @@ use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\Response;
 use App\utils\database;
 use GuzzleHttp\Client;
+use Symfony\Component\Security\Csrf\CsrfToken;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 
 class TicketsOrderController extends AbstractController
 {
@@ -16,27 +18,47 @@ class TicketsOrderController extends AbstractController
      * @Route("/ticketsorder", name="tickets_order")
      */
 
-    public function ticketsOrders(Request $request)
+    public function ticketsOrders(Request $request, CsrfTokenManagerInterface $csrfTokenManager)
     {
 
-    
+        $this->csrfTokenManager = $csrfTokenManager;
+       
+        if ($request->isMethod('post')){
+            $csrftoken= $request->request->get('_csrf_token');
+            if (!$this->isCsrfTokenValid('authenticate', $csrftoken)) 
+            {
+                return $this->redirectToRoute('error403');
+            }
+            $showDateTime= $request->request->get('datum');
+            $session = $request->getSession();
+            $session->set('showDateTime', $showDateTime);
+           
 
-        $session = $request->getSession();
-        $filmID = $session->get('filmId');
-        $filmTitle = $session->get('filmTitle');
-        $client = new Client(['base_uri' => 'http://fullstacksyntra.be/cockpitdfs/api/', 'timeout' => 2.0, ]);
+            $session = $request->getSession();
+            $filmID = $session->get('filmId');
+            $filmTitle = $session->get('filmTitle');
+            $showDateTime = $session->get('showDateTime');
 
-            $req = $client->request(
-                'GET',
-                'collections/get/filmposters',
-                ['headers' => ['Cockpit-Token' => '9e5c1c03342c45edccdfb83095f054'],
-                    'json' => ['filter' => [ 'Id' => $filmID]]
-                ]
-            );
-            $answer = json_decode($req->getBody()->getContents());
-            $val = $answer->entries[0]->Image->path;
-        
-        return $this->render('pages/ticketorder.html.twig', array(
-            'filmTitle' => $filmTitle, 'moviePoster' => $val));
+            $db = new database();
+            $playtime = $db->getAuditoriumInfo($filmID, $showDateTime);
+            $auditoriumname = $playtime["auditorium_name"];
+            
+            $client = new Client(['base_uri' => 'http://fullstacksyntra.be/cockpitdfs/api/', 'timeout' => 2.0, ]);
+
+                $req = $client->request(
+                    'GET',
+                    'collections/get/filmposters',
+                    ['headers' => ['Cockpit-Token' => '9e5c1c03342c45edccdfb83095f054'],
+                        'json' => ['filter' => [ 'Id' => $filmID]]
+                    ]
+                );
+                $answer = json_decode($req->getBody()->getContents());
+                $val = $answer->entries[0]->Image->path;
+            
+            
+            return $this->render('pages/ticketorder.html.twig', array(
+                'filmTitle' => $filmTitle, 'moviePoster' => $val, 'showDateTime' => $showDateTime, 'auditorium' => $auditoriumname));
+            }
+
     }
 }
